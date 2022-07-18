0f61a6e21c04e7998fe87748a6937ac311343f36
Roman Nazarenko <justaspeckinspace@gmail.com>
2022-07-18 21:36:15 +0300

    refactor: remane 2 classes and add some code
    Rename the ExceptionHandler class to an ErrorHandler class.
    Rename the ExceptionResponse class to an ErrorResponse class.
    Add description field to ErrorResponse class.

---
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ErrorHandler.java
@@ -0,0 +1,16 @@
+package ru.yandex.practicum.filmorate.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.yandex.practicum.filmorate.model.ErrorResponse;
+
+@RestControllerAdvice("ru.yandex.practicum")
+public class ErrorHandler {
+    @ExceptionHandler
+    //@ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleValidationException(final ValidationException exception) {
+        return new ErrorResponse("Ошибка валидации!", exception.getMessage());
+    }
+}
--- src/main/java/ru/yandex/practicum/filmorate/exception/ExceptionHandler.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.exception;
-
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-@RestControllerAdvice("ru.yandex.practicum")
-public class ExceptionHandler {
-}
