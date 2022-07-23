9d5d5dd61bef29fcf1bcb50ccba9b64e41d11df5
Roman Nazarenko <justaspeckinspace@gmail.com>
2022-07-23 19:48:11 +0300

    refactor: change User class constructor

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import lombok.NonNull;
 
 import java.time.LocalDate;
+import java.util.HashSet;
 import java.util.Set;
 
 @Data
@@ -24,5 +25,5 @@ public class User {
     private final LocalDate birthday;
     @NonNull
     private String name;
-    private Set<Long> friends;
+    private Set<Long> friends = new HashSet<>();
 }
